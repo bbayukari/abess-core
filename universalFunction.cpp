b5efdd71e1c7831fa8c2982788e146b7711f7c4a
bbayukari <40588568+bbayukari@users.noreply.github.com>
2022-04-01 14:26:13 +0800

    update

--- universalFunction.cpp
+++ universalFunction.cpp
@@ -1,3 +1,5 @@
+#ifdef R_BUILD // this file is only used wich Rcpp
+
 // [[Rcpp::depends(BH)]]
 // [[Rcpp::depends(StanHeaders)]]
 #include <stan/math.hpp>
@@ -5,13 +7,13 @@
 #include "UniversalData.h"
 
 // [[Rcpp::plugins(cpp14)]]
-
-
+using namespace Eigen;
+// TODO: Define a global variable ExternData and no longer pass data pointer
 struct ExternData{
 /************************************** User defined **************************************/
-    Eigen::MatrixXd x;
-    Eigen::VectorXd y;
-    ExternData(Eigen::MatrixXd x, Eigen::VectorXd y) {
+    MatrixXd x;
+    VectorXd y;
+    ExternData(MatrixXd x, VectorXd y) {
         this->x = x;
         this->y = y;
     }
@@ -21,27 +23,28 @@ struct ExternData{
 
 class AutoDiffFunction :public UniversalData {
 private:
-    const Eigen::VectorXd effective_para;
+    VectorXd const *effective_para_ptr;
 public:
-    AutoDiffFunction(const UniversalData& universal_data, const Eigen::VectorXd& effective_para):UniversalData(universal_data), effective_para(effective_para) {
+    AutoDiffFunction(const UniversalData& universal_data, VectorXd const *effective_para_ptr)
+        :UniversalData(universal_data), effective_para_ptr(effective_para_ptr)
+    {
         // assert(this->effective_para.size() == this->effective_para_index.size());
-        if (this->compute_para_index.size() == 0) {
-            this->compute_para_index = this->effective_para_index;
+        if (this->compute_para_index_ptr == NULL) {
+            this->compute_para_index_ptr = &this->effective_para_index;
         }
-        this->data = (ExternData*)this->data;
     }
     template <typename T> 
-    T operator()(const Eigen::VectorX<T>& compute_para) const {
-        // assert(this->compute_para_index.size() == compute_para.size());
-        Eigen::VectorX<T> para(this->dim);
+    T operator()(const VectorX<T>& compute_para) const {
+        ExternData*  data = static_cast<ExternData*>(this->data);
+        VectorX<T> para(this->model_size);
         for (int i = 0; i < para.size(); i++) {
             para[i] = T(0.0);
         }
         for (int i = 0; i < this->effective_para_index.size(); i++) {
-            para[this->effective_para_index[i]] = T(effective_para[i]);
+            para[this->effective_para_index[i]] = T((*this->effective_para_ptr)[i]);
         }
-        for (int i = 0; i < this->compute_para_index.size(); i++) {
-            para[this->compute_para_index[i]] = compute_para[i];
+        for (int i = 0; i < this->compute_para_index_ptr->size(); i++) {
+            para[(*this->compute_para_index_ptr)[i]] = compute_para[i];
         }
 /************************************** User defined **************************************/
         return (data->x * para - data->y).cwiseAbs2().sum();
@@ -49,36 +52,36 @@ public:
     }
 };
 
-double comput_value(const Eigen::VectorXd& effective_para, const UniversalData& universal_data) {
-    AutoDiffFunction function(universal_data, effective_para);
-    Eigen::VectorXd compute_para;
+double comput_value(const VectorXd& effective_para, const UniversalData& universal_data) {
+    AutoDiffFunction function(universal_data, &effective_para);
+    VectorXd compute_para;
     function.get_compute_para(effective_para, compute_para);
     return function(compute_para);
 }
 
-double compute_gradient(const Eigen::VectorXd& effective_para, const UniversalData& universal_data, Eigen::VectorXd* gradient) {
+double compute_gradient(const VectorXd& effective_para, const UniversalData& universal_data, VectorXd* gradient) {
     double value = 0;
-    AutoDiffFunction function(universal_data, effective_para);
-    Eigen::VectorXd compute_para;
+    AutoDiffFunction function(universal_data, &effective_para);
+    VectorXd compute_para;
     function.get_compute_para(effective_para, compute_para);
     stan::math::gradient(function, compute_para, value, *gradient);
     return value;
 }
 
-double compute_hessian(const Eigen::VectorXd& effective_para, const UniversalData& universal_data, Eigen::VectorXd* gradient, Eigen::MatrixXd* hessian) {
+double compute_hessian(const VectorXd& effective_para, const UniversalData& universal_data, VectorXd* gradient, MatrixXd* hessian) {
     double value = 0;  
-    AutoDiffFunction function(universal_data, effective_para);
-    Eigen::VectorXd compute_para;
+    AutoDiffFunction function(universal_data, &effective_para);
+    VectorXd compute_para;
     function.get_compute_para(effective_para, compute_para);
     if (!gradient) {
-        auto grad = Eigen::VectorXd(compute_para.size());
+        auto grad = VectorXd(compute_para.size());
         gradient = &grad;
     }
     stan::math::hessian(function, compute_para, value, *gradient, *hessian);
     return value;
 }
 
-double function(const Eigen::VectorXd& effective_para, const UniversalData& universal_data, Eigen::VectorXd* gradient, Eigen::MatrixXd* hessian) {
+double function(const VectorXd& effective_para, const UniversalData& universal_data, VectorXd* gradient, MatrixXd* hessian) {
     if (hessian) {
         return compute_hessian(effective_para, universal_data, gradient, hessian);
     }
@@ -96,9 +99,12 @@ Rcpp::XPtr<universal_function> get_universal_function() {
 // [[Rcpp::export]]
 Rcpp::XPtr<void*> get_extern_data(
     /************************************** User defined **************************************/
-    Eigen::MatrixXd x, Eigen::VectorXd y
+    MatrixXd x, VectorXd y
     /************************************** User defined **************************************/
 ) {
     ExternData* ptr = new ExternData(x,y);
     return Rcpp::XPtr<void*>(new (void*) ptr);
 }
+
+
+#endif // R_BUILD
